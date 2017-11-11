24b189f1c5c98f370d9e6e35ea9de9cecbbe5fa4
Ron Snijders <ron.snijders@tno.nl>
2017-11-11 14:01:35 +0100

    Now able to render kde.

--- kde/kernel_density_estimation.py
+++ kde/kernel_density_estimation.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
+from scipy import stats
 import argparse
 import logging
 
 import pandas as pd
 
 from rdwgs_converter import RDWGSConverter
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import imread
+import matplotlib.cbook as cbook
+
+
+conv = RDWGSConverter()
+
+min_lat = 53.2068
+max_lon = 6.5930
+min_x, min_y = conv.fromWgsToRd((min_lat, max_lon))
+
+max_lat = 53.2276
+min_lon = 6.5417
+max_x, max_y = conv.fromWgsToRd((max_lat, min_lon))
 
 
 def read_tsv(path):
@@ -22,6 +39,33 @@ def read_tsv(path):
     return df
 
 
+def plot(df):
+    #np.random.seed(0)
+    #x = np.random.uniform(0.0,10.0,15)
+    #y = np.random.uniform(0.0,10.0,15)
+
+    datafile = 'osm_map.png'
+    img = imread(datafile)
+    #plt.scatter([],[],zorder=1)
+    plt.scatter(df["x"],df["y"],zorder=1)
+
+    axes = plt.gca()
+    axes.set_ylim([min_y,max_y])
+    axes.set_xlim([min_x,max_x])
+
+    X, Y = np.mgrid[min_x:max_x:100j, min_y:max_y:100j]
+    positions = np.vstack([X.ravel(), Y.ravel()])
+    values = np.vstack([df["x"], df["y"]])
+    kernel = stats.gaussian_kde(values)
+    Z = np.reshape(kernel(positions).T, X.shape)
+
+    plt.imshow(img, zorder=0, extent=[min_x, max_x, min_y, max_y])
+    plt.imshow(np.rot90(Z), cmap=plt.cm.gist_earth_r, extent=[min_x, max_x, min_y, max_y], alpha=0.5)
+    #plt.imshow(img, zorder=0)
+    #plt.imshow(img, zorder=0)
+    plt.show()
+
+
 def main(args=None):
     parser = argparse.ArgumentParser(description='Applies and visualizes the kernel density estimation applied to the bike thefts.')
     parser.add_argument('file', help='Path the the bike theft file in tsv format.', type=str)
@@ -32,6 +76,8 @@ def main(args=None):
     df = read_tsv(args.file)
     if not args.write_file is None:
         df.to_csv(args.write_file, sep="\t")
+
+    plot(df)
 
 
 if __name__ == "__main__":
