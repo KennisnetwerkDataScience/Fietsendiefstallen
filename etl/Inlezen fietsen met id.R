58679b58b13f27febd4a13eeb862332c9021fdb6
Gerard Wolters <gerard@MacBook-Pro-van-Gerard.local>
2017-11-16 15:39:52 +0100

    Inlezen fietsen met ID ipv Bronregistratie

---
+++ etl/Inlezen fietsen met id.R
@@ -0,0 +1,15 @@
+# Inlezen en opschonen/categoriseren fietsenbestand
+
+library(readxl)
+
+fietsen <- read_excel('data/Fietsen incident id.xlsx') %>% 
+  select(-1) %>%
+  mutate(soort = ifelse(grepl('ATB|CROSS|LIG|RACE|SPORT', trefwoord), 'sport', 
+                        ifelse(trefwoord == 'DAMES', 'dames',
+                               ifelse(trefwoord == 'HEREN', 'heren',
+                                      ifelse(trefwoord == 'OPOE', 'opoe',
+                                             ifelse(grepl('TRISCH', trefwoord), 'electrisch',
+                                                    ifelse(is.na(trefwoord), NA, 'overig')))))))
+
+fietsen %>%
+  write.csv('data/fietsen_id.csv')
