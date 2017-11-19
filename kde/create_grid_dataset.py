c3f2b3958bf9e4483c7e9e87a80d6a096e262fa7
Ron Snijders <ron.snijders@tno.nl>
2017-11-19 22:48:23 +0100

    Added lights.

--- kde/create_grid_dataset.py
+++ kde/create_grid_dataset.py
@@ -31,7 +31,8 @@ min_x, min_y = conv.fromWgsToRd((min_lat, min_lon))
 max_x, max_y = conv.fromWgsToRd((max_lat, max_lon))
 
 
-def read_tsv(path):
+def read_theft_tsv(path):
+    logging.info("Reading file: %s" % path)
     converters = {}
     df = pd.read_csv(path, sep="\t", encoding="ISO-8859-1", converters=converters)
 
@@ -49,20 +50,35 @@ class GridFeatureExtractor():
 
     def __init__(self, grid):
         self._grid = grid
-        self._grid_features = {c:{"count":0} for c in range(grid * grid)}
+        self._cel_max = grid * grid - 1
+        self._grid_features = {c:{"count":0, "lights":0} for c in range(grid * grid)}
         self._box_width = (max_x - min_x) / self._grid
         self._box_height = (max_y - min_y) / self._grid
+        self._conv = RDWGSConverter()
 
     def to_cel_index(self, x, y):
         """ Returns the index of the grid cel providing the x and y coordinates. """
         x_pos = int((x - min_x) / self._box_width)
         y_pos = int((y - min_y) / self._box_height)
-        return y_pos * self._grid + x_pos
+        index = y_pos * self._grid + x_pos
+        return None if index > self._cel_max else index
 
     def set_y(self, df):
         for k, r in df.iterrows():
             cel = self.to_cel_index(r["x"], r["y"])
             self._grid_features[cel]["count"] += 1
+
+    def add_lights(self, df):
+        logging.info("Adding %d lights." % len(df))
+        for k, r in df.iterrows():
+            x, y = self._conv.fromWgsToRd((r["y"], r["x"]))
+            cel = self.to_cel_index(x, y)
+            #Add if cel is not outside grid:
+            if not cel is None:
+                self._grid_features[cel]["lights"] += 1
+
+    def get_features(self):
+        return self._grid_features
 
 
 def assign_grid(df, grid):
@@ -85,11 +101,16 @@ def main(args=None):
     parser.add_argument('--grid', type=int, default=30, help='If set uses the specified value as the widht/height of the grid to assign the rows to.')
     args = parser.parse_args(args)
 
-    logging.info("Reading file: %s" % args.file)
-    df = read_tsv(args.file)
+    df = read_theft_tsv(args.file)
 
     e = GridFeatureExtractor(args.grid)
     e.set_y(df)
+    if not args.lights is None:
+        lights_df = pd.read_csv(args.lights, sep=",", encoding="ISO-8859-1")
+        columns = lights_df.columns.values
+        logging.debug("Light columns found: %s" % str(columns))
+        e.add_lights(lights_df)
+    print(e.get_features())
 
 
 if __name__ == "__main__":
